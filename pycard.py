206e863517064defb6400819055b626aff808019
Justin Snyder <ifoundthebeef@gmail.com>
2019-05-20 02:11:44 -0600

    Make it work with yaml instead of csv

--- pycard.py
+++ pycard.py
@@ -1,174 +1,233 @@
-from jinja2 import Template
-import os
-from optparse import OptionParser
-import logging
+import argparse
 import csv
-import time
+import logging
+import os
 import re
+import time
+
+import livereload
+import yaml
+import jinja2
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler, FileSystemEventHandler
-from itertools import zip_longest
-from livereload import Server
+
+log = logging.getLogger('pycard')
 
 
-VERSION = '0.1.0'
+VERSION = '0.2.0'
 
 RENDERED_CARDS_FILE = "index.html"
 
+FULL_DECK_TEMPLATE = os.path.join(os.path.dirname(__file__), 'cards.html.jinja2')
 
-class CardRenderer:
-    def __init__(self, input_path, prefix):
-        self.prefix = prefix
-        self.input_path = input_path
 
-        self.csv_card_path = self.get_path("csv")
-        self.custom_header_path = self.get_path("header.html")
-        self.single_card_template_path = self.get_path("html.jinja2")
+def render_from_csv(csv_path, delimiter=','):
+    base, ext = os.path.splitext(csv_path)
+    card_template_path = base + '.html.jinja2'
+    header_path = base + '.header.html'
+    # load the single card template
+    with open(card_template_path) as tf:
+        template = jinja2.Template(tf.read())
 
-        self.cards_template_path = os.path.join(os.path.dirname(__file__), 'cards.html.jinja2')
+    # load the csv file
+    rendered_cards = []
+    with open(csv_path, encoding='utf-8-sig') as csvfile:
+        for card in csv.DictReader(csvfile, delimiter=delimiter):
+            if str(card.get('ignore', "false")).lower() == "true":
+                continue
 
-        self.all_cards_rendered_path = os.path.join(input_path, RENDERED_CARDS_FILE)
+            rendered = template.render(
+                card,
+                __card_data=card,
+                __time=str(time.time())
+            )
+            copies = card.get('copies', 1)
+            if not copies.isdigit():
+                copies = 1
 
-    def get_path(self, extension):
-        return os.path.join(self.input_path, "{}.{}".format(self.prefix, extension))
+            for i in range(int(copies)):
+                rendered_cards.append(rendered)
 
-    def render_cards(self):
-        # I've noticed that when saving the CSV file
-        # the server reloads an empty page
-        # unless I add a small sleep before attempting to read everything
-        time.sleep(0.5)
+    # Load custom header html if it exists
+    custom_header = None
 
-        # load the csv file
-        cards_data = []
-        with open(self.csv_card_path, "r", encoding='utf-8-sig') as csvfile:
-            reader = csv.DictReader(csvfile, dialect='custom_delimiter')
-            for row in reader:
-                cards_data.append(row)
+    if os.path.exists(header_path):
+        with open(header_path) as f:
+            custom_header = f.read()
 
-        rendered_cards = []
+    # render the cards template with all rendered cards
+    with open(FULL_DECK_TEMPLATE) as tf:
+        template = jinja2.Template(tf.read())
 
-        # load the single card template
-        with open(self.single_card_template_path, "r") as template_file:
-            template = Template(template_file.read())
+    with open(output_path, "w") as of:
+        of.write(
+            template.render(
+                rendered_cards=rendered_cards,
+                stylesheet=os.basename(base) + '.css',
+                custom_header=custom_header
+            )
+        )
 
-            # render the template with card data
-            for card_data in cards_data:
-                if str(card_data.get('ignore', "false")).lower() == "true":
-                    continue
 
-                rendered = template.render(
-                    card_data,
-                    __card_data=card_data,
-                    __time=str(time.time())
-                )
-                num_cards = card_data.get('num_cards')
-                if num_cards is None or re.match("^[^0-9]*$", num_cards):
-                    num_cards = 1
+def render_from_yaml(yaml_path):
+    base, ext = os.path.splitext(os.path.basename(yaml_path))
+    source_dir = os.path.dirname(yaml_path)
 
-                num_cards = int(num_cards)
-                for i in range(0, int(num_cards)):
-                    rendered_cards.append(rendered)
+    with open(yaml_path) as yf:
+        deck_info = yaml.safe_load(yf)
 
-        # Load custom header html if it exists
+    global_ = {
+        'template': FULL_DECK_TEMPLATE,
+        'stylesheet': base + '.css',
+        'header': base + '.header.html',
+        'output': base + '.html',
+        **deck_info.get('global', {})
+    }
+    defaults = {
+        'template': base + '.html.jinja2',
+        'copies': 1,
+        **deck_info.get('default', {})
+    }
+
+    template_cache = {}
+    def get_template(name):
+        nonlocal template_cache
+        if not name.endswith('.html.jinja2'):
+            name += '.html.jinja2'
+        if name in template_cache:
+            return template_cache[name]
+        else:
+            with open(os.path.join(source_dir, name)) as tf:
+                template = jinja2.Template(tf.read())
+            template_cache[name] = template
+            return template
+
+    rendered_cards = []
+    for card in deck_info['cards']:
+        try:
+            copies = int(card.pop('copies', defaults['copies']) or '1')
+        except ValueError as err:
+            log.warning("Invalid value for 'copies': {}".format(err.args[0]))
+            copies = 1
+
+        if copies <= 0:
+            continue
+
+        log.info(card)
+        template = get_template(card.pop('template', defaults['template']))
+        rendered = template.render(
+            {**defaults, **card},
+            __card_data=card,
+            __time=str(time.time())
+        )
+        log.info(rendered)
+        rendered_cards += [rendered] * copies
+
+    log.info("%d total cards", len(rendered_cards))
+
+    header_path = os.path.join(source_dir, global_['header'])
+    if os.path.exists(header_path):
+        with open(header_path) as f:
+            custom_header = f.read()
+    else:
         custom_header = None
 
-        if os.path.exists(self.custom_header_path):
-            with open(self.custom_header_path, "r") as f:
-                custom_header = f.read()
+    with open(global_['template']) as tf:
+        global_template = jinja2.Template(tf.read())
 
-        # render the cards template with all rendered cards
-        with open(self.cards_template_path, "r") as cards_template_file:
-            template = Template(cards_template_file.read())
-            with open(self.all_cards_rendered_path, "w") as all_cards_rendered_file:
-                all_cards_rendered_file.write(
-                    template.render(
-                        rendered_cards=rendered_cards,
-                        prefix=self.prefix,
-                        custom_header=custom_header
-                    )
-                )
+    with open(os.path.join(source_dir, global_['output']), "w") as of:
+        of.write(
+            global_template.render(
+                rendered_cards=rendered_cards,
+                stylesheet=global_['stylesheet'],
+                custom_header=custom_header
+            )
+        )
+    return source_dir, global_['output']
 
 
 class RenderingEventHandler(FileSystemEventHandler):
-    def __init__(self, card_renderer):
-        self.card_renderer = card_renderer
+    def __init__(self, render_func, ignore=()):
+        self._render_func = render_func
+        self._ignored_files = ignore
 
     def on_any_event(self, event):
-        if event.src_path == self.card_renderer.all_cards_rendered_path:
+        if event.src_path in self._ignored_files:
             return
 
-        self.card_renderer.render_cards()
+        time.sleep(0.5)  # wait for the file to be written
+        self._render_func()
 
 
-def parse_options():
-    parser = OptionParser(
-        usage="usage: %prog [options]",
-        version="%prog {}".format(VERSION)
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="HTML + CSS card template renderer"
     )
-    parser.add_option("-p", "--path",
-                      help="path to assets",
-                      dest="path",
-                      default=os.getcwd(),
-                      metavar="PATH")
 
-    parser.add_option("-x", "--prefix",
-                      help="filename prefix, example _card<.ext>",
-                      dest="prefix",
-                      default="_card",
-                      metavar="PREFIX")
+    parser.add_argument(
+        'path',
+        type=os.path.abspath,
+        metavar='PATH',
+        help="path to assets"
+    )
 
-    parser.add_option("-d", "--delimiter",
-                      help="delimiter used in the csv file, default: , (comma)",
-                      dest="delimiter",
-                      default=",",
-                      metavar="DELIMITER")
+    parser.add_argument(
+        '-p', '--port',
+        type=int,
+        default=8800,
+        metavar='PORT',
+        help="port to use for live reloaded page",
+    )
 
-    parser.add_option("--port",
-                      help="port to use for live reloaded page",
-                      dest="port",
-                      type="int",
-                      default=8800,
-                      metavar="PORT")
+    parser.add_argument(
+        '--host',
+        help="host address to bind to",
+        default='0.0.0.0',
+        metavar='ADDRESS'
+    )
 
-    parser.add_option("--address",
-                      help="host address to bind to",
-                      dest="host_address",
-                      default="0.0.0.0",
-                      metavar="ADDRESS")
+    parser.add_argument(
+        '-1', '--no-server',
+        dest='run_server',
+        action='store_false',
+        help="Do not start up a server that watches the directory"
+    )
 
     return parser.parse_args()
 
 
 def main():
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
 
-    (options, args) = parse_options()
+    args = parse_args()
 
-    port = options.port
-    assets_path = options.path
-    file_prefix = options.prefix
-    host_address = options.host_address
+    source, output = render_from_yaml(args.path)
 
-    csv.register_dialect('custom_delimiter', delimiter=options.delimiter)
+    if args.run_server:
+        observer = Observer()
+        observer.schedule(
+            LoggingEventHandler(),
+            source,
+            recursive=True
+        )
+        observer.schedule(
+            RenderingEventHandler(lambda: render_from_yaml(args.path), output),
+            source,
+            recursive=True
+        )
 
-    card_renderer = CardRenderer(assets_path, file_prefix)
+        observer.start()
 
-    observer = Observer()
-    observer.schedule(LoggingEventHandler(), assets_path, recursive=True)
-    observer.schedule(RenderingEventHandler(card_renderer), assets_path, recursive=True)
+        server = livereload.Server()
+        server.watch(source)
+        server.serve(root=source, port=args.port, host=args.host)
 
-    card_renderer.render_cards()
-
-    observer.start()
-
-    server = Server()
-    server.watch(card_renderer.all_cards_rendered_path)
-    server.serve(root=assets_path, port=port, host=host_address)
-
-    observer.stop()
-    observer.join()
+        observer.stop()
+        observer.join()
 
 
 if __name__ == "__main__":
